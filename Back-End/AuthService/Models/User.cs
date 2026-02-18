d81653d3c27e47de9bc03a6b3d7600dccf97203c
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-02-18 23:50:48 +0000

    Back-End : Prepared Our Controller file, And Defined Our User Model's Scheme

---
+++ Back-End/AuthService/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthService.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public String Email { get; set; }
+        public String PasswordHash { get; set; }
+        public String Role { get; set; } = "Customer"; // Default Value
+        public DateTime CreatedAt { get; set; }
+    }
+}
