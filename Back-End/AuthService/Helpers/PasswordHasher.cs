0e0383a93390bb1f7b0119883fd40c7fd20409cb
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-02-19 15:59:05 +0000

    Back-End : Added The Folder 'Helpers' That (Will) Contain(s) Some Helpfull Classes That Will Be UseFull Later On During The Implementation Of Our Services

---
+++ Back-End/AuthService/Helpers/PasswordHasher.cs
@@ -0,0 +1,18 @@
+﻿using BCrypt.Net;
+namespace AuthService.Helpers
+{
+    public static class PasswordHasher
+    {
+        // The Method Responsable For Hashing A Given Password
+        public static String HashPassword(String password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        // The Method Responsable For Verifying A Given Password With Its Hashed Version
+        public static bool VerifyPassword(String password, String hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
