418364f204987fe2a637eac61bab67c61fda54d6
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-02-19 22:09:54 +0000

    Back-End : Declared The Main Functions That Will Be Called Out Inside The Controller That'll Handle The Authentication Proccesses

--- Back-End/AuthService/Services/IAuthService.cs
+++ Back-End/AuthService/Services/IAuthService.cs
@@ -1,6 +1,12 @@
-﻿namespace AuthService.Services
+﻿using AuthService.DTOs;
+
+namespace AuthService.Services
 {
-    public class IAuthService
+    public interface IAuthService
     {
+        Task RegisterProcess(RegisterRequest request);
+        Task<String> LoginProcess(LoginRequest request);
+        Task VerifyEmailExistence(EmailVerificationRequest request);
+        Task UpdateOldPassword(PasswordChangeRequest request);
     }
 }
