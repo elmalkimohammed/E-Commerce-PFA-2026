83cc635470b7b9691b33dc1d4500fc6aafaade37
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-02-20 22:19:54 +0000

    Back-End : Started Working On The Kafka Side To Handle Publishing Events In Order To Communicate With Other Micro-Services. Created The 'Events' Folder Which Contains The Model Of The Event Of When A User Is Successfully Rgistered

---
+++ Back-End/AuthService/Events/UserRegisteredEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthService.Events
+{
+    public class UserRegisteredEvent
+    {
+        public Guid UserId { get; set; }
+        public String Email { get; set; }
+        public String Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
