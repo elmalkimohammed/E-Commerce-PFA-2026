c2ed48fd556e5931922544468162af134b9581dd
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-02-21 15:58:36 +0000

    Back-End : Due To Many Issues Regarding Docker And DataBase Accessiblity, I've Migrated To MySQL, Which Meant Changing The Querries To Fit The MySQL Syntaxe And The Connection String Too In The 'appsettings.json'. Also Added The AuthService's Database As Docker Container

--- Back-End/AuthService/Repository/UserRepository.cs
+++ Back-End/AuthService/Repository/UserRepository.cs
@@ -1,38 +1,39 @@
 ﻿using AuthService.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
 namespace AuthService.Repository
 {
     public class userRepository : IUserRepository
     {
-        // Connection String for SQL Server Database
+        // Connection String for MySQL Database
         private readonly String _connectionString;
         public userRepository(IConfiguration configuration)
         {
             this._connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
-        // The Method Responsable For Getting The User's Infos Through Their Email
+        // The Method Responsible For Getting The User's Info Through Their Email
         public async Task<User> GetThroughEmail(String email)
         {
             // Open The Database Connection
-            using var connection = new SqlConnection(this._connectionString);
+            using var connection = new MySqlConnection(this._connectionString);
             await connection.OpenAsync();
 
-            // Sql Command To Get The User Through Email
-            var cmd = new SqlCommand("SELECT * FROM Users WHERE Email = @email", connection);
+            // MySQL Command To Get The User Through Email
+            var cmd = new MySqlCommand("SELECT * FROM Users WHERE Email = @email", connection);
             cmd.Parameters.AddWithValue("@email", email);
 
-            // Executing The Sql Command
+            // Executing The MySQL Command
             using var reader = await cmd.ExecuteReaderAsync();
 
             // Checking If The Email Doesn't Exist
-            if( !reader.Read() )
+            if( !await reader.ReadAsync() )
             {
                 return null;
             }
 
-            // Returing The Found User Object
+            // Returning The Found User Object
             return new User {
                 UserId = reader.GetGuid(0),
                 Email = reader.GetString(1),
@@ -42,38 +43,38 @@ namespace AuthService.Repository
             };
         }
 
-        // Method Responsable For Adding A New User To The Database
+        // Method Responsible For Adding A New User To The Database
         public async Task CreateUser(User user)
         {
             // Open The Database Connection
-            using var connection = new SqlConnection(this._connectionString);
+            using var connection = new MySqlConnection(this._connectionString);
             await connection.OpenAsync();
 
-            // Sql Command To Insert The New User Into The Database
-            var cmd = new SqlCommand("INSERT INTO Users (UserId, Email, PasswordHash, Role, CreatedAt) VALUES (@userid, @email, @passwordhash, @role, @createdat)", connection);
+            // MySQL Command To Insert The New User Into The Database
+            var cmd = new MySqlCommand("INSERT INTO Users (UserId, Email, PasswordHash, Role, CreatedAt) VALUES (@userid, @email, @passwordhash, @role, @createdat)", connection);
             cmd.Parameters.AddWithValue("@userid", user.UserId);
             cmd.Parameters.AddWithValue("@email", user.Email);
             cmd.Parameters.AddWithValue("@passwordhash", user.PasswordHash);
             cmd.Parameters.AddWithValue("@role", user.Role);
             cmd.Parameters.AddWithValue("@createdat", user.CreatedAt);
 
-            // Executing The Sql Command
+            // Executing The MySQL Command
             await cmd.ExecuteNonQueryAsync();
         }
 
-        // The Method Responsable For Updating The Passsword Of A User
+        // The Method Responsible For Updating The Password Of A User
         public async Task UpdateForgottenPassword(Guid userId, String newPassword)
         {
             // Open The Database Connection
-            using var connection = new SqlConnection(this._connectionString);
+            using var connection = new MySqlConnection(this._connectionString);
             await connection.OpenAsync();
 
-            // Sql Command To Update The Old Password According To The Given UserId
-            var cmd = new SqlCommand("UPDATE Users SET PasswordHash = @password WHERE UserId = @userId", connection);
+            // MySQL Command To Update The Old Password According To The Given UserId
+            var cmd = new MySqlCommand("UPDATE Users SET PasswordHash = @password WHERE UserId = @userId", connection);
             cmd.Parameters.AddWithValue("@password", newPassword);
             cmd.Parameters.AddWithValue("@userId", userId);
 
-            // Executing The Sql Command
+            // Executing The MySQL Command
             await cmd.ExecuteNonQueryAsync();
         }
     }
