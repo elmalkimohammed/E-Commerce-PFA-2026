a27aabb7eac2fc3093b8e5cdbae6190cdf7ae5c9
@mesmesamin42 <mesmesamin42@gmail.com>
2026-02-25 20:59:44 +0000

    Ajout de la page profil

---
+++ Profil_Page_Pfa/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ProfilePage from './pages/ProfilePage';
+import './App.css'; // Gardez votre CSS existant
+
+function App() {
+  return <ProfilePage />;
+}
+
+export default App;
