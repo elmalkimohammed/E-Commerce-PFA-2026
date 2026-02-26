c86be0f51e5275f97b0c5709d0d0039744a68f9c
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-02-26 13:44:35 +0000

    Forgot To Commit The Deleted Files/Folders, So I Did Just That

--- Profil_Page_Pfa/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ProfilePage from './pages/ProfilePage';
-import './App.css'; // Gardez votre CSS existant
-
-function App() {
-  return <ProfilePage />;
-}
-
-export default App;
