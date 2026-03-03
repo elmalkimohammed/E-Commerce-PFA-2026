a8b700c1071edf2fc5a7d116ccb29696057f7591
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-03-03 23:29:18 +0000

    Back-End : Declared Our Main Service's Methodes, And Also Added A DTO Containing Two DTOs Inside To Handle Responses Better

---
+++ Back-End/CartService/DTOs/CartResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace CartService.DTOs
+{
+    public class CartResponse
+    {
+        public class CartResponse
+        {
+            public Guid CartId { get; set; }
+            public Guid UserId { get; set; }
+            public List<CartItemDto> Items { get; set; }
+        }
+
+        public class CartItemDto
+        {
+            public int ProductId { get; set; }
+            public int Stock { get; set; }  
+        }
+    }
+}
