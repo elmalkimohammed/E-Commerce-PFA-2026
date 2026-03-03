f0df6f4d3d0e839745879dcbfe76cedd9ae67bf2
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-03-03 23:42:14 +0000

    Back-End : Declared Our Main Repository Methodes In The Interface

---
+++ Back-End/CartService/Services/ICartRepository.cs
@@ -0,0 +1,14 @@
+﻿using CartService.DTOs;
+using CartService.Models;
+namespace CartService.Services
+{
+    public interface ICartRepository
+    {
+        Task<Cart> GetUserCart_ThroughID(Guid userId);
+        Task CreateCart_ForUser(Guid userId);
+        Task AddItem_ForCart(Guid userId, AddToCartRequest re);
+        Task UpdateStock_ForCartItem(Guid userId, UpdateStockRequest req);
+        Task RemoveItem_FromCart(Guid userId, Guid productId);
+        Task RemoveAll_FromCart(Guid userId);
+    }
+}
