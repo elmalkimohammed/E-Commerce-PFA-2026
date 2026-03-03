195b92183d39badd149992a3bb9bc8963510c233
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-03-03 21:36:32 +0000

    Back-End & Docker : Prepared The Service And Its Database To Be Launched With Docker, And Also Deleted The Default Files In The .NET. And Also Prepared The Structure For The .NET Project

---
+++ Back-End/CartService/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Carts (
+    CartId CHAR(36) PRIMARY KEY,
+    UserId CHAR(36) NOT NULL,
+    CreatedAt DATETIME NOT NULL
+);
+
+CREATE TABLE CartItems (
+    CartItemId CHAR(36) PRIMARY KEY,
+    CartId CHAR(36) NOT NULL,
+    ProductId CHAR(36) NOT NULL,
+    Quantity INT NOT NULL,
+    FOREIGN KEY (CartId) REFERENCES Carts(CartId)
+);
