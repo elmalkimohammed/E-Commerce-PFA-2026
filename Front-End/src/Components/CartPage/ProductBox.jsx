889a88d77353d80ac0fb45ae579697b05ad39bf4
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-03-03 17:58:41 +0000

    Front-End : Made The '+' & '-' Buttons' Logic Function As Intended. The User Can't Add More Than What We Have In Stock

--- Front-End/src/Components/CartPage/ProductBox.jsx
+++ Front-End/src/Components/CartPage/ProductBox.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react"
+
 import "../styles/ProductBox.css"
 
 function ProductBox( { productMimeType , productImage , productName , productDescription , productPrice , productMaxStock } ) {
     
     const imgSrc = `data:${ productMimeType };base64,${ productImage }`
+
+    const [ stockCounter , setStockCounter ] = useState(0)
+
+    const stockAddition = async () => {
+        if ( stockCounter < productMaxStock ) {
+            setStockCounter(stockCounter + 1)
+        }
+    }
+
+    const stockSubstraction = async () => {
+        if ( stockCounter > 0 ) {
+            setStockCounter(stockCounter-1)
+        }
+    }
 
     return(
         <div className="prodBoxContainer" style={ { height: "fit-content" } }>
@@ -18,9 +34,9 @@ function ProductBox( { productMimeType , productImage , productName , productDes
             </div>
             <div className="rightSide">
                 <div className="stockControl">
-                    <button className="subs">-</button>
-                    <p>2</p>
-                    <button className="add">+</button>
+                    <button className="subs" onClick={ stockSubstraction }>-</button>
+                    <p>{ stockCounter }</p>
+                    <button className="add" onClick={ stockAddition }>+</button>
                 </div>
                 <button style={ { background: "transparent", borderStyle: "none", border: "1px solid black", fontWeight: "bolder", width: "fit-content", alignSelf: "flex-end", padding: "10px", borderRadius: "5px", color: "#143a63", cursor: "pointer" } }>Retirer</button>
             </div>
