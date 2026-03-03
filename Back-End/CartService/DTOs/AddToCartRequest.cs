7f62ac076880d468a3e32ada89f4bb8707686d60
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-03-03 22:32:42 +0000

    Back-End : Decided Our DTOs

---
+++ Back-End/CartService/DTOs/AddToCartRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CartService.DTOs
+{
+    public class AddToCartRequest
+    {
+        public int ProductId { get; set; }
+        public int Stock { get; set; }  
+    }
+}
--- Back-End/CartService/init.sql
+++ Back-End/CartService/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE Carts (
 CREATE TABLE CartItems (
     CartItemId CHAR(36) PRIMARY KEY,
     CartId CHAR(36) NOT NULL,
-    ProductId CHAR(36) NOT NULL,
+    ProductId SMALLINT NOT NULL,
     Quantity INT NOT NULL,
     FOREIGN KEY (CartId) REFERENCES Carts(CartId)
 );
