84a088cf49c3134d834c325f48a8d36c735a1d07
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-03-03 23:17:11 +0000

    Back-End : Declared Our Models While Also Defining The Attribute That Represents The (One To Many) Relation Between The Two Tables

---
+++ Back-End/CartService/Models/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace CartService.Models
+{
+    public class Cart
+    {
+        public Guid CartId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<CartItem> Items { get; set; }
+    }
+}
---
+++ Back-End/CartService/Models/CartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace CartService.Models
+{
+    public class CartItem
+    {
+        public Guid CartItemId { get; set; }
+        public Guid CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Stock { get; set; }
+    }
+}
