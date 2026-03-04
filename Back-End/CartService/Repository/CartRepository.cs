d2e73b62fbce2a5335f28ef6a08c53adcd524c35
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-03-04 01:33:12 +0000

    Back-End : Implemented Both Of The Methodes Responsable For Creating A New Cart For A User And Getting A Cart For One

--- Back-End/CartService/Repository/CartRepository.cs
+++ Back-End/CartService/Repository/CartRepository.cs
@@ -2,6 +2,7 @@
 using CartService.Models;
 using CartService.Services;
 using Microsoft.AspNetCore.Http.HttpResults;
+using MySql.Data.MySqlClient;
 
 namespace CartService.Repository
 {
@@ -16,12 +17,68 @@ namespace CartService.Repository
         // Get The Entire Cart Through The UserId
         public async Task<Cart> GetUserCart_ThroughID(Guid userId)
         {
-            return Ok();
+            // Initializing The Connection To The Database
+            using var conn = new MySqlConnection(this._connString);
+            await conn.OpenAsync();
+            // MySql Querry Against Sql Injection Attack For Getting The Specific Cart Of The User
+            var cmdCart = new MySqlCommand("SELECT * FROM Carts WHERE UserId = @userId", conn);
+            cmdCart.Parameters.AddWithValue("@userId", userId.ToString());
+            // Executing The Querry And Return A Null Value When Failing
+            using var reader = await cmdCart.ExecuteReaderAsync();
+            if ( !reader.HasRows)
+            {
+                return null;
+            }
+            await reader.ReadAsync();
+            // Converting Incomming Data Into Processable DataTypes
+            var cart = new Cart
+            {
+                CartId = Guid.Parse(reader["CartId"].ToString()!),
+                UserId = Guid.Parse(reader["UserId"].ToString()!),
+                CreatedAt = Convert.ToDateTime(reader["CreatedAt"])
+            };
+            reader.Close();
+            // MySql Querry Against The Sql Injection Attack For Getting The Items Of The Cart
+            var cmdItems = new MySqlCommand("SELECT * FROM CartItems WHERE  CartId = @cartId", conn);
+            cmdItems.Parameters.AddWithValue("@cartId", cart.CartId.ToString());
+            // Executing The Querry
+            using var itemsReader = await cmdItems.ExecuteReaderAsync();
+            while ( await itemsReader.ReadAsync())
+            {
+                cart.Items.Add
+                (
+                    new CartItem
+                    {
+                        CartItemId = Guid.Parse(itemsReader["CartItemsId"].ToString()),
+                        CartId = Guid.Parse(itemsReader["CartItemsId"].ToString()),
+                        ProductId = Convert.ToInt32(itemsReader["ProductId"]),
+                        Stock = Convert.ToInt32(itemsReader["Quantity"])
+                    }
+                )
+            }
+            // Return The Entire Cart
+            return cart;
         }
         // Create A Cart For The User
         public async Task CreateCart_ForUser(Guid userId)
         {
-            return Ok();
+            // Initializing The Cart Variable
+            var newCart = new Cart
+            {
+                CartId = Guid.NewGuid(),
+                UserId = userId,
+                CreatedAt = DateTime.UtcNow
+            }
+            // Opening The Connection To The Database
+            using var conn = new MySqlConnection(this._connString);
+            await conn.OpenAsync();
+            // The MySql Querry Against The Sql Injection Attack For Inserting The New Cart Into The Database
+            var cmdNewCart = new MySqlCommand("INSERT INTO Carts (CartId, UserId, CreatedAt) VALUES (@cartId, @userId, @createdAt)", conn);
+            cmdNewCart.Parameters.AddWithValue("@userId", newCart.CartId);
+            cmdNewCart.Parameters.AddWithValue("@cartId", newCart.UserId);
+            cmdNewCart.Parameters.AddWithValue("@createdAt", newCart.CreatedAt);
+            // Executing The MySql Querry
+            cmdNewCart.ExecuteNonQueryAsync();
         }
         // Add A Product To The Cart
         public async Task AddItem_ForCart(Guid userId , AddToCartRequest req)
