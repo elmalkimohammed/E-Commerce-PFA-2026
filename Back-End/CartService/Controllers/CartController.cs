47385c2421b3437dec15f077f15f97908668dd8e
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-03-05 00:56:20 +0000

    Back-End : Finished Implementing The Controller With All Of Its Routes

--- Back-End/CartService/Controllers/CartController.cs
+++ Back-End/CartService/Controllers/CartController.cs
@@ -1,4 +1,5 @@
-﻿using CartService.Services;
+﻿using CartService.DTOs;
+using CartService.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,20 +22,59 @@ namespace CartService.Controllers
         }
         private Guid GetUserId()
         {
-            // Gathering The UserId From The JWT Token
+            // Extracting The UserId From The JWT Token
             var userIdClaim = User.FindFirst( JwtRegisteredClaimNames.Sub ) ?? User.FindFirst( ClaimTypes.NameIdentifier ) ;
             if ( userIdClaim == null)
             {
                 throw new Exception("The User Id Wasn't Found In The Token!");
             } else
             {
+                // Converting The Given UserId Into A Proper GUID Structure
                 return Guid.Parse(userIdClaim.Value);
             }
         }
         [HttpGet("getCart")]
+        public async Task<IActionResult> GettingFullCart()
+        {
+            var userId = this.GetUserId();
+            var cart = await this._cartService.GetUser_Cart(userId);
+            return Ok(cart);
+        }
         [HttpPost("addToCart")]
+        public async Task<IActionResult> AddingItem(AddToCartRequest request)
+        {
+            if ( request == null || request.Stock <= 0)
+            {
+                return BadRequest("Invalid Request!");
+            }
+            var userId = this.GetUserId();
+            await this._cartService.AddItem_ToCart(userId, request);
+            return Ok( new { message = "Product Added To The Cart Successfully!" } );
+        }
         [HttpPut("updateStock")]
-        [HttpDelete("deleteItem")]
+        public async Task<IActionResult> UpdateQuantity(UpdateStockRequest request)
+        {
+            if ( request == null)
+            {
+                return BadRequest("Invalid Request!");
+            }
+            var userId = this.GetUserId();
+            await this._cartService.EditStock_FromCart(userId, request);
+            return Ok( new { message = "Quantity Updated Successfully!" } );
+        }
+        [HttpDelete("deleteItem/{productId}")]
+        public async Task<IActionResult> DeleteItem(int productId)
+        {
+            var userId = this.GetUserId();
+            await this._cartService.DeleteItem_FromCart(userId, productId);
+            return Ok( new { message = "Item Deleted Successfully!" } );
+        }
         [HttpPost("clearCart")]
+        public async Task<IActionResult> ClearCart()
+        {
+            var userId = this.GetUserId();
+            await this._cartService.ClearAll_OfCart(userId);
+            return Ok( new { message = "The Cart Has Been Cleared!" } );
+        }
     }
 }
