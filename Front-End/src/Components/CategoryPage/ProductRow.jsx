2fe75d4274f21aaf06d1673ee419eb5aeb9eb5fc
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-03-14 23:42:01 +0000

    Front-End : Finished Preparing The Product's Row Component

--- Front-End/src/Components/CategoryPage/ProductRow.jsx
+++ Front-End/src/Components/CategoryPage/ProductRow.jsx
@@ -1,9 +1,21 @@
 import "../styles/productRow.css"
 
-function ProductRow() {
+function ProductRow({ prodName , prodDesc , prodPrice }) {
     return(
         <>
-            
+            <div className="prodRox">
+                <div className="leftInfos">
+                    <img src="" alt="Image De Produit" />
+                    <div className="desc_name">
+                        <h1>{ prodName }</h1>
+                        <p>{ prodDesc }</p>
+                    </div>
+                </div>
+                <div className="rightInfos">
+                    <h1>{ prodPrice }</h1>
+                    <button>Détails</button>
+                </div>
+            </div>
         </>
     )
 }
