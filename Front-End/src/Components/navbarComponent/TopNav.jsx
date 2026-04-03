6ff4c6f185a4e102cb8dd335c9037fc95084b4c4
med amine bouchadad <78551050+med-amine0629@users.noreply.github.com>
2026-04-03 17:16:23 +0100

    Fixed The Problem Realted Categories Sliders Not Showing - Oussama Rah Mzroub

--- Front-End/src/Components/navbarComponent/TopNav.jsx
+++ Front-End/src/Components/navbarComponent/TopNav.jsx
@@ -13,7 +13,7 @@ function TopNav() {
 
   const navChecker = () => {
     if ( localStorage.getItem("generatedJWT_Token") ) navigate("/ProfilePage")
-    else navigate("/Authentication")
+    else navigate("/Authentication") 
   }
 
   /* Fetching All Of The Available Categories From The API */
