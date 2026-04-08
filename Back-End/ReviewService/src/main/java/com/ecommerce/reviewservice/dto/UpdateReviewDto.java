ac884da917bbc925a467cc3fc2b38e518e91ba09
Oussama <ouss.ko2000@gmail.com>
2026-04-08 19:45:49 +0100

    Revert "SE53Payment_form/cart/Pages"
    This reverts commit 8055c31ca7fedf27731983727442b7f0d2bd9e93, reversing
    changes made to 6fca21125df58bad7e65db8eadd6cf48f9ed1038.

--- Back-End/ReviewService/src/main/java/com/ecommerce/reviewservice/dto/UpdateReviewDto.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.reviewservice.dto;
-
-import jakarta.validation.constraints.*;
-
-public record UpdateReviewDto(
-        @Min(1) @Max(5) Integer rating,
-        String comment
-) {}
