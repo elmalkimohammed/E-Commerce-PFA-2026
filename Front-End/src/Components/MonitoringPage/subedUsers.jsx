4a0517019138ec9b03191a54cb4ca7ee87b055b1
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-04-14 01:27:28 +0000

    Front-End : Completely Done With The Monitoring Page By Finishing The Last Remaining Component That's Needed For Rendering The Subbed Users

--- Front-End/src/Components/MonitoringPage/subedUsers.jsx
+++ Front-End/src/Components/MonitoringPage/subedUsers.jsx
@@ -1,10 +1,30 @@
 
+import SubbedUsersRow from "./Inside Components/subbedUsersRow"
+import "../styles/adminMonitoringGlobalStyles.css"
 
 function SubbedUsers() {
     return(
-        <>
-            <div>hi from subbed users</div>
-        </>
+        <table className="MonitoringGlobalStyles">
+            <tbody>
+                <tr>
+                    <td>Id D'inscription</td>
+                    <td>Id D'utilisateur</td>
+                    <td>Id De Plan</td>
+                    <td>Date De Fin</td>
+                    <td style={ { textTransform: "capitalize" } } >état</td>
+                    <td>Plan Choisie</td>
+                </tr>
+                <SubbedUsersRow subId={1} userId={1} planId={2} endDate={"15-09-2024"}
+                    status={"Cancelled"} subscriptionPlan={"Premium"}
+                />
+                <SubbedUsersRow subId={1} userId={1} planId={2} endDate={"15-09-2024"}
+                    status={"Cancelled"} subscriptionPlan={"Premium"}
+                />
+                <SubbedUsersRow subId={1} userId={1} planId={2} endDate={"15-09-2024"}
+                    status={"Cancelled"} subscriptionPlan={"Premium"}
+                />
+            </tbody>
+        </table>
     )
 }
 
