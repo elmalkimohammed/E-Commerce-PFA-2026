4c51b6120356263f033787ab7a2a717125a6e35f
elmalkimohammed <mohammedelmalki241@gmail.com>
2026-04-14 00:56:38 +0000

    Front-End : Finished The Component For Displaying The Connected Users And Their Full Infos

--- Front-End/src/Components/MonitoringPage/connectedUsers.jsx
+++ Front-End/src/Components/MonitoringPage/connectedUsers.jsx
@@ -1,9 +1,37 @@
 
+import ConnRow from "./Inside Components/connsRow"
+
+import "../styles/adminMonitoringGlobalStyles.css"
+
 function ConnectedUsers() {
     return(
-        <>
-            <div>hi from connected users</div>
-        </>
+        <table className="MonitoringGlobalStyles">
+            <tbody>
+                <tr>
+                    <td>Id D'utilisateur</td>
+                    <td>Email</td>
+                    <td>Nom</td>
+                    <td>Prenom</td>
+                    <td>Téléphone</td>
+                    <td>Addresse</td>
+                    <td>Gendre</td>
+                    <td>Date De Naissance</td>
+                    <td>Image De Profile</td>
+                </tr>
+                <ConnRow userId={1} Email={"elmalki@gmail.com"} FirstName={"elmalki"}
+                    LastName={"mohammed"} Phone={"0678452169"} Address={"Hay Al Fath"} Sex={"Homme"}
+                    DateOfBirth={"15-02-2005"} ProfileImage={"image/test.jpg"}
+                />
+                <ConnRow userId={1} Email={"elmalki@gmail.com"} FirstName={"elmalki"}
+                    LastName={"mohammed"} Phone={"0678452169"} Address={"Hay Al Fath"} Sex={"Homme"}
+                    DateOfBirth={"15-02-2005"} ProfileImage={"image/test.jpg"}
+                />
+                <ConnRow userId={1} Email={"elmalki@gmail.com"} FirstName={"elmalki"}
+                    LastName={"mohammed"} Phone={"0678452169"} Address={"Hay Al Fath"} Sex={"Homme"}
+                    DateOfBirth={"15-02-2005"} ProfileImage={"image/test.jpg"}
+                />
+            </tbody>
+        </table>
     )
 }
 
